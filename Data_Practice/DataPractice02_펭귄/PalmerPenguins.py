07f11a6b1471184a028f85d8e7f7d431a7855cad
honeydanji <tjdwls3680@naver.com>
2023-07-05 17:48:35 +0900

    펭귄종분류

--- Data_Practice/DataPractice02_펭귄/PalmerPenguins.py
+++ Data_Practice/DataPractice02_펭귄/PalmerPenguins.py
@@ -1,8 +1,10 @@
 import seaborn as sns
 import pandas as pd
+import xgboost as xgb
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
-### 목표 : 펭귄의 종을 예측하는 모델을 만들어라!!!
+### 목표 : 펭귄의 종을 분류하는 모델을 만들어라!!!
 
 ## 데이터셋 준비
 penguins = sns.load_dataset('penguins') 
@@ -33,8 +35,25 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, stratif
 # y = 'species' 로 지정했다. 데이터 안에 존재하는 펭귄의 종의 수만큼 클래스가 생성된다.
 # 각각의 클래스에 분포되는 테스트용 데이터와 훈련용 데이트의 수는 다를지 몰라도 비율은 동일하게 설정된다.
 
-## POINT
+### POINT
 # 그럼 왜? 펭귄 종마다 클래스를 나누는 것일까? >> 범주형모델이기 때문이다.
 # 즉 데이터의 범위를 정해 놓으면 새로운 펭귄데이터(인스턴스)가 입력이 될 때 쉽게 펭귄의 종류를 예측할 수 있기 때문이다.
 # 결론 : 원본 데이터에서 타겟변수를 정하고, 남은 데이터를 이용해 타겟 변수의 범주를 정한다. <<< 이게 핵심이다.
 #       여기서 타겟변수는 예측 대상이 되는 것! ex) 펭귄의 종을 예측하세요., 꽃의 종류를 예측하세요. 등등 
+
+## 기준 설정? (무슨 기준)
+## XGBoost (1. XGBoost 분류자 인스턴스 생성, 2. 하이퍼파라미터 설정)
+xgb_classifier = xgb.XGBClassifier(
+    objective="binary:logistic", # 이진분류
+    max_depth=3, # 트리의 최대 깊이 3
+    learning_rate=0.1, # 학습률 지정 (예시로는 : 경사하강법을 생각해라. 학습률이 높으면 빠르지만 오버슈팅이 된다.)
+    n_estibators=100 # 트리의수 ?? 추정기 수?? 
+                     # 스무고개 게임을 생각해라. 질문이 많을수록 정답에 가까워 질 수 있지만, 필요하지 않은 질문을 하면 전혀다른 답이 나올 수 있다.
+                     # 트리 즉 추정기는 적당해야한다.
+) 
+
+
+
+
+### 개념
+# 하이퍼파라미터 : 변경이 가능한 변수로 학습률, 에코프(훈련반복횟수), 가중치 초기화 등이 있다.
