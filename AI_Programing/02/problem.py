9f94dde700c11d44aac58b560df6f4aa842e5579
honeydanji <tjdwls3680@naver.com>
2023-07-06 14:31:51 +0900

    파일 수정

---
+++ AI_Programing/02/problem.py
@@ -0,0 +1,16 @@
+import math
+import random
+
+# interface
+class Problem:
+    def __init__(self) -> None:
+        pass
+        
+class Numeric(Problem):
+    def __init__(self):
+        Problem.__init__()
+        
+        
+class Tsp(Problem):
+    def __init__(self) -> None:
+        Problem.__init__()
